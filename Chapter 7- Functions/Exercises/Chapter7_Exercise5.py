37ffd3784928b584c8f2a79f79394d65b1439307
Sandy Sarte <114162003+Sandy-smora@users.noreply.github.com>
2022-10-26 12:08:22 +0400

    Create Chapter7_Exercise5.py

---
+++ Chapter 7- Functions/Exercises/Chapter7_Exercise5.py
@@ -0,0 +1,11 @@
+## Exercise 5: Cities
+
+def cities(city, country):
+    msg = f"{city.title()} is a city in {country.title()}."
+    print(msg)
+
+cities("Bern", "Switzerland")
+cities("Khovd", "Mongolia")
+cities("Miri", "Malaysia")
+cities("Limerick", "Ireland")
+cities("Nantes", "France")
