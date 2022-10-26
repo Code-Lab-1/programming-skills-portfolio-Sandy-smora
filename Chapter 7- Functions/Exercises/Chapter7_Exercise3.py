bac50faeb964e7d89287b9a0e725d29696ecac6d
Sandy Sarte <114162003+Sandy-smora@users.noreply.github.com>
2022-10-26 12:08:03 +0400

    Create Chapter7_Exercise3.py

---
+++ Chapter 7- Functions/Exercises/Chapter7_Exercise3.py
@@ -0,0 +1,7 @@
+## Exercise 3: T-Shirt  :ballot_box_with_check:
+
+def tshirt(size, message):
+    print(f"\nI'll get a {size} t-shirt.")
+    print(f'It will say, "{message}"')
+
+tshirt(message = "Sorry I'm late, I didn't want to attend", size = "extra large")
