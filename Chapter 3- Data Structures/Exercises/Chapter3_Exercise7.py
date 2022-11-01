3f2035096ae5311984a39d9ae11b5989c9e73c9d
Sandy Sarte <114162003+Sandy-smora@users.noreply.github.com>
2022-11-01 09:19:11 +0400

    Create Chapter3_Exercise7.py

---
+++ Chapter 3- Data Structures/Exercises/Chapter3_Exercise7.py
@@ -0,0 +1,42 @@
+## Exercise 7: Seeing the World :ballot_box_with_check:
+
+five_places = ["Keukenhof, Netherlands", "Butterfly Garden, Dubai", "Furano, Japan", "Ibaraki, Japan", "Zweisimmen, Switzerland"]
+
+print("Original:")
+print(five_places)
+
+print()
+print("Alphabetical:")
+print(sorted(five_places))
+
+print()
+print("Original:")
+print(five_places)
+
+print()
+print("Reverse alphabetical order")
+print(sorted(five_places, reverse=True))
+
+print()
+print("Original:")
+print(five_places)
+
+print()
+print("Reverse:")
+five_places.reverse()
+print(five_places)
+
+print()
+print("Original:")
+five_places.sort()
+print(five_places)
+
+print()
+print("Alphabetical:")
+five_places.sort()
+print(sorted(five_places))
+
+print()
+print("Reverse alphabetical order")
+five_places.sort(reverse=True)
+print(five_places)
