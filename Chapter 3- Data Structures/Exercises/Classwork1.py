4288fa3aadca12a84b9f078f165a45633deaf596
Sandy Sarte <114162003+Sandy-smora@users.noreply.github.com>
2022-11-24 09:24:11 +0400

    Create Classwork1.py

---
+++ Chapter 3- Data Structures/Exercises/Classwork1.py
@@ -0,0 +1,16 @@
+ppl_names = ["Sandy", "Alexis", "Sandybouy", "Timothy", "Alexah", "Alexander", "Morita", "Philip", "Stefani", "Vic"]
+print(ppl_names)
+
+# printing list (diff range or only one)
+print(ppl_names[5])
+print(ppl_names[0:6])
+print(ppl_names[-1])
+print(ppl_names[3:])
+print(ppl_names[:7])
+
+ppl_names[7] = "Bob"
+print(ppl_names)
+
+# inserting a new variable in list
+ppl_names.insert (1, "April")
+print(ppl_names)
