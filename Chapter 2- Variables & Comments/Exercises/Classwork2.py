75b7f8f579cb23d436f4d8fcf00aa3912a599c6d
Sandy Sarte <114162003+Sandy-smora@users.noreply.github.com>
2022-11-24 09:23:38 +0400

    Create Classwork2.py

---
+++ Chapter 2- Variables & Comments/Exercises/Classwork2.py
@@ -0,0 +1,9 @@
+# printing multiple strings
+name = "Sandy Sarte"
+age = 16
+contact_number = "0507201343"
+email = "sandysarte@yahoo"
+department = "Creative Computing"
+level = "Year 1"
+
+print(name, age, contact_number, email, department,)
