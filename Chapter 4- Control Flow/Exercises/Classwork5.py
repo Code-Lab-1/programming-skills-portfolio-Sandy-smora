408f53b288545bb28a454acc6d83486baabd4ab9
Sandy Sarte <114162003+Sandy-smora@users.noreply.github.com>
2022-11-24 09:25:12 +0400

    Create Classwork5.py

---
+++ Chapter 4- Control Flow/Exercises/Classwork5.py
@@ -0,0 +1,6 @@
+# id user are allowed to drive
+age=int(input("Please enter your age here: "))
+if age >=18:
+    print("You are allowed to drive")
+else:
+    print("You are NOT allowed to drive, you are a child")
